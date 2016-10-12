8111f998a32038f2942108c401417e7718a79578
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-10-12 00:41:31 -0300

    Update MainActivity

--- app/src/main/java/com/example/guilhermedeoliveira/popularseries/view/MainActivity.java
+++ app/src/main/java/com/example/guilhermedeoliveira/popularseries/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.guilhermedeoliveira.popularseries.R;
 import com.example.guilhermedeoliveira.popularseries.api.SerieService;
@@ -17,9 +18,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BASE_URL = "https://api.trakt.tv/";
+    public static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
-    public static final String BASE_URL = "https://api.trakt.tv/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
         SerieService service = retrofit.create(SerieService.class);
 
-        Call<Serie> call = service.getPopularSeries();
+        Call<Serie> call = service.getPopularSeries(int page, int limit);
         call.enqueue(new Callback<Serie>() {
             @Override
             public void onResponse(Call<Serie> call, Response<Serie> response) {
-
+                if (response.isSuccessful()) {
+                    SerieService service = (SerieService) response.body();
+                } else {
+                    Log.i(TAG, "Erro: " + response.code());
+                }
             }
 
             @Override
             public void onFailure(Call<Serie> call, Throwable t) {
-
+                Log.e(TAG, "Erro: " + t.getMessage());
             }
         });
 
