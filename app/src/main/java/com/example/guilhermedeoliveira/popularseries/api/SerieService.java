366cb204bdd71fc2918fc75f2adaa23a27d485b9
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-10-12 00:41:09 -0300

    Update SerieService

--- app/src/main/java/com/example/guilhermedeoliveira/popularseries/api/SerieService.java
+++ app/src/main/java/com/example/guilhermedeoliveira/popularseries/api/SerieService.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
+
+import static com.example.guilhermedeoliveira.popularseries.api.SerieService.CLIENT_ID;
+import static com.example.guilhermedeoliveira.popularseries.api.SerieService.VERSION;
 
 /**
  * Created by guilhermeoliveira on 10/10/16.
@@ -14,16 +18,20 @@ import retrofit2.http.Query;
 
 public interface SerieService {
 
+    int VERSION = 2;
     public static final String CLIENT_ID =
             "30bbec5288d3c9018ad9b9aef2460651782e0c081a1ef0d77288e20d58b6b343";
 
-    // @GET("movies/popular")
-    @GET("shows/popular")
-    Call<List<Serie>> getPopularSeries();
-    //Call<List<Serie>> getPopularSeries(@Query("page") Integer pageNo, @Query("limit") Integer limit);
+    @Headers({
+            "Content-Type:application/json",
+            "trakt-api-version:" + VERSION,
+            "trakt-api-key:" + CLIENT_ID
+    })
+
+    @GET("shows/popular?extended=images")
+    Call<List<Serie>> getPopularSeries(@Query("page") int page , @Query("limit") int limit);
 
     // testing with cUrl
-
     /**
      * shows:
      * curl --include --head-header
