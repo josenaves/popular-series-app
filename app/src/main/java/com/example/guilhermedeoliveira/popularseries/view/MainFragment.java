7443a66ddd1623b08a14dd71c737a1736754a1d0
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-10-13 00:51:35 -0300

    Update Detail Fragment

---
+++ app/src/main/java/com/example/guilhermedeoliveira/popularseries/view/MainFragment.java
@@ -0,0 +1,22 @@
+package com.example.guilhermedeoliveira.popularseries.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by guilhermeoliveira on 13/10/16.
+ */
+
+public class MainFragment extends Fragment {
+
+    // API retrofit task will come to fragment
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+}
