b0bd44414fd442796db6480f3c70b6dfc7a7ba9c
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-10-13 00:50:27 -0300

    Create Detail Fragment

---
+++ app/src/main/java/com/example/guilhermedeoliveira/popularseries/view/DetailFragment.java
@@ -0,0 +1,20 @@
+package com.example.guilhermedeoliveira.popularseries.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by guilhermeoliveira on 13/10/16.
+ */
+
+public class DetailFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+}
