819c11c752a1b41c2aad91c6b4da66327c5a4719
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2016-10-13 00:47:31 -0300

    Create Detail Activity

---
+++ app/src/main/java/com/example/guilhermedeoliveira/popularseries/view/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.guilhermedeoliveira.popularseries.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guilhermedeoliveira.popularseries.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
